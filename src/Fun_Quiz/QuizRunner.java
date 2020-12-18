c2cf937b776f6a05e66589e9980089f5cfba708f
tarabyte84 <tekotthoff@me.com>
2020-12-17 20:09:39 -0600

    Worked more on QuizRunner. Added loop for asking questions and adding to right and wrong answer accumulators.

--- src/Fun_Quiz/QuizRunner.java
+++ src/Fun_Quiz/QuizRunner.java
@@ -1,6 +1,7 @@
 package Fun_Quiz;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class QuizRunner {
 
@@ -28,18 +29,79 @@ public class QuizRunner {
             "2) Purple", "3) Brown", "4) Green", 2, 4);
 
         //Add quiz items to colorQuiz
-        colorQuiz.addItem(multi1);
-        colorQuiz.addItem(multi2);
+//        colorQuiz.addItem(multi1);
+//        colorQuiz.addItem(multi2);
         colorQuiz.addItem(tf1);
         colorQuiz.addItem(tf2);
-        colorQuiz.addItem(cb1);
-        colorQuiz.addItem(cb2);
+//        colorQuiz.addItem(cb1);
+//        colorQuiz.addItem(cb2);
 
-        System.out.println(colorQuiz.size());
-        System.out.println(colorQuiz.get(0));
+//        System.out.println(colorQuiz.size());
+//        System.out.println(colorQuiz.get(0));
 //        System.out.println(colorQuiz.get(0).answerToString());
 
+//        for (int question: colorQuiz) {
+//            System.out.println(question);
+//        }
+//        System.out.println(((TrueFalse) colorQuiz.get(3)).isTrue());
 
+        int numCorrectAnswers = 0;
+        int numWrongAnswers = 0;
+
+        for (int i = 0; i < colorQuiz.size(); i++) {
+            Scanner input;
+            Scanner input2;
+            System.out.println(colorQuiz.get(i));
+            
+            if(colorQuiz.get(i) instanceof MultipleChoice) {
+                int userAnswer;
+                input = new Scanner(System.in);
+                System.out.println("Please enter the corresponding number for the correct answer:");
+                userAnswer = input.nextInt();
+                if(userAnswer == ((MultipleChoice)colorQuiz.get(i)).getCorrectAnswer()) {
+                    numCorrectAnswers += 1;
+                } else {
+                    numWrongAnswers += 1;
+                }
+                
+            } else if(colorQuiz.get(i) instanceof CheckBox) {
+                int userAnswer1;
+                int userAnswer2;
+                input = new Scanner(System.in);
+                input2 = new Scanner(System.in);
+                System.out.println("There are two correct answers. Please enter the corresponding number for the first" +
+                        "correct answer and hit enter, then enter the second corresponding number.");
+                userAnswer1 = input.nextInt();
+                userAnswer2 = input2.nextInt();
+                if (userAnswer1 == ((CheckBox) colorQuiz.get(i)).getCorrectAnswer1() || userAnswer1 ==
+                        ((CheckBox) colorQuiz.get(i)).getCorrectAnswer2()) {
+                    numCorrectAnswers += 1;
+                } else if (userAnswer2 == ((CheckBox) colorQuiz.get(i)).getCorrectAnswer1() || userAnswer2 ==
+                        ((CheckBox) colorQuiz.get(i)).getCorrectAnswer2()) {
+                    numWrongAnswers += 1;
+                }
+
+            } else if(colorQuiz.get(i) instanceof TrueFalse) {
+                input = new Scanner(System.in);
+                System.out.println("Please enter 1 to indicate 'True' and 2 to indicate 'False'.");
+                int userAnswer = input.nextInt();
+                if(userAnswer == 1 || ((TrueFalse) colorQuiz.get(i)).isTrue()) {
+                    numCorrectAnswers += 1;
+                } else if(userAnswer == 1 || !((TrueFalse) colorQuiz.get(i)).isTrue()) {
+                    numWrongAnswers += 1;
+                } else if(userAnswer == 2 || !((TrueFalse) colorQuiz.get(i)).isTrue()) {
+                    numCorrectAnswers += 1;
+                } else if(userAnswer == 2 || ((TrueFalse) colorQuiz.get(i)).isTrue()) {
+                    numWrongAnswers += 1;
+                }
+
+            }
+
+
+        }
+
+        System.out.println(numCorrectAnswers);
+        System.out.println(numWrongAnswers);
 
 
     }
